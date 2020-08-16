9258a2a2a7ef68e43c2b3465c75abe22aab8bca3
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-16 20:19:24 -0300

    Área do Círculo
    Área do Círculo

---
+++ uri1002/uri1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1002
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double area, n, raio;
+
+			n = 3.14159;
+			raio = double.Parse(Console.ReadLine());
+			area = n * Math.Pow(raio,2);
+
+			Console.WriteLine("A=" + area.ToString("F4"));
+		}
+	}
+}
