30e7a2f51fe26ccf01f27af3568ca4d053673062
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-17 20:57:17 -0300

    Consumo
    Consumo

---
+++ uri1014/uri1014/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, consumoMedio;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine());
+            consumoMedio = X / Y;
+
+            Console.WriteLine(consumoMedio.ToString("F3") + " km/l");
+        }
+    }
+}
