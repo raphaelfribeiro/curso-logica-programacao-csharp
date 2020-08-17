37042886b7e5a16f3c8dbb7051057ed611c1e575
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-17 19:56:20 -0300

    Salário
    Salário

---
+++ uri1008/uri1008/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario, numeroHorasTrabalhadas;
+            double valorHora, salario;
+
+            numeroFuncionario = int.Parse(Console.ReadLine());
+            numeroHorasTrabalhadas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine());
+
+            salario = (numeroHorasTrabalhadas * valorHora);
+
+            Console.WriteLine("NUMBER = " + numeroFuncionario);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2"));
+        }
+    }
+}
