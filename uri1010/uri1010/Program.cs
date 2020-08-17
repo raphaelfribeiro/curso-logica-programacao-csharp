200c59e4a896473a449561ca14314a7923517531
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-17 20:40:35 -0300

    Cálculo Simples
    Cálculo Simples

---
+++ uri1010/uri1010/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            string[] vet1, vet2;
+            
+            vet1 = Console.ReadLine().Split(' ');
+            vet2 = Console.ReadLine().Split(' ');
+            valor = (double.Parse(vet1[1]) * double.Parse(vet1[2])) + (double.Parse(vet2[1]) * double.Parse(vet2[2]));
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + valor.ToString("F2"));            
+        }
+    }
+}
