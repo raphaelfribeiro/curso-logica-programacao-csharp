efdc6fdcfda20c6ae899a32c03f0b8128ef1cf3e
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 20:05:53 -0300

    Média 2
    Média 2

---
+++ uri1006/uri1006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, media;
+
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+            media = (((A * 2) + (B * 3) + (C * 5)) / (2 + 3 + 5));
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1"));
+        }
+    }
+}
