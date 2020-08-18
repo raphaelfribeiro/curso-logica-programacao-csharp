89d9421fab594c89b859585979775baf0e8f7056
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-17 21:20:57 -0300

    Gasto de Combustível
    Gasto de Combustível

---
+++ uri1017/uri1017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoGasto, velocidadeMedia;
+            double quantidadeLitros;
+
+            tempoGasto = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+            quantidadeLitros = ((double)tempoGasto * (double)velocidadeMedia) / 12;
+
+            Console.WriteLine(quantidadeLitros.ToString("F3"));
+        }
+    }
+}
