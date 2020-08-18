8aeb54e9ad6d3de31a24ae469496d259b49002eb
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 20:31:28 -0300

    Salário com Bônus
    Salário com Bônus

---
+++ uri1009/uri1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioFixo, totalVendas, totalFinal;
+
+            Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine());
+            totalVendas = double.Parse(Console.ReadLine());
+            totalFinal = salarioFixo + (totalVendas * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + totalFinal.ToString("F2"));
+        }
+    }
+}
