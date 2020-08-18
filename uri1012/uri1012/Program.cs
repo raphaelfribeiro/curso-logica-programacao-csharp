7cb87a5418c608b6bef3d316f18edf44a5a9d9ef
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 20:59:16 -0300

    Área
    Área

---
+++ uri1012/uri1012/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, pi, triangulo, circulo, trapezio, quadrado, retangulo;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            A = double.Parse(vet[0]);
+            B = double.Parse(vet[1]);
+            C = double.Parse(vet[2]);
+            pi = 3.14159;
+
+            triangulo = (A * C) / 2.0;
+            circulo = pi * Math.Pow(C, 2.0);
+            trapezio = ((A + B) / 2.0) * C;
+            quadrado = Math.Pow(B, 2.0);
+            retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3"));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3"));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3"));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3"));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3"));
+        }
+    }
+}
