9ba312a582d75d960be2506d0fa72d7889776397
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 20:40:11 -0300

    Esfera
    Esfera

---
+++ uri1011/uri1011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, pi, volume;
+
+            R = double.Parse(Console.ReadLine());
+            pi = 3.14159;
+            volume = (4 / 3.0) * pi * Math.Pow(R, 3.0);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+        }
+    }
+}
