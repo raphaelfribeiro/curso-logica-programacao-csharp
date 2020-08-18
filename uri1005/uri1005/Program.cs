4d4650f991c924658b1d5a2902077c4dede71df1
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 19:59:19 -0300

    Média 1
    Média 1

---
+++ uri1005/uri1005/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1, valor2, media;
+
+            valor1 = double.Parse(Console.ReadLine());
+            valor2 = double.Parse(Console.ReadLine());
+            media = ((valor1 * 3.5) + (valor2 * 7.5)) / (3.5 + 7.5);
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5"));
+        }
+    }
+}
