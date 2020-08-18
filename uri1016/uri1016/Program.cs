71bded8061dc48466dcdbabbe09487495a6f0aa2
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-17 21:06:18 -0300

    Distância
    Distância

---
+++ uri1016/uri1016/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine((int.Parse(Console.ReadLine()) * 2) + " minutos");
+        }
+    }
+}
