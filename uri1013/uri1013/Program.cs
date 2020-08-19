79047031df51b8d6e931c7481520ef3e063e507d
Raphael Ribeiro <raphaelfribeiro@usp.br>
2020-08-18 21:24:01 -0300

    O Maior
    O Maior

---
+++ uri1013/uri1013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, maiorAB, maiorABC;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+            maiorABC = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+            Console.WriteLine(maiorABC + " eh o maior");
+        }
+    }
+}
